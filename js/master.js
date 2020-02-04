b5bcf8e24d8a84d7793bea5b49cdf1610bdfda7c
Derek Townsend <derekjavon2@gmail.com>
2020-02-04 16:57:54 -0600

    Basics Added and first video page added

--- js/master.js
+++ js/master.js
@@ -1,7 +1,20 @@
 // alert("hello")
 
+function navigation() {
+  let icon = document.getElementById("icon");
+  let nav= document.getElementById("myTopNav");
+
+  icon.addEventListener("click",() =>{
+    if (!nav.classList.contains("responsive")){
+      nav.classList.add("responsive");
+    } else{
+      nav.classList.remove("responsive")
+    }
+
+  });
+}
 
 function run() {
-
+  navigation()
 }
 run()
