41d076bc0c198a4042d9d16aae09b2a0e0f757f8
Derek Townsend <derekjavon2@gmail.com>
2020-03-24 02:40:42 -0500

    navigation complete

--- js/master.js
+++ js/master.js
@@ -2,7 +2,10 @@
 
 function navigation() {
   let icon = document.getElementById("icon");
-  let nav= document.getElementById("myTopNav");
+  let nav = document.getElementById("myTopNav");
+  let lessonList = document.getElementById("lesson-nav");
+  let articleList = document.getElementById("article-nav");
+  let hiddenItems = document.querySelectorAll(".hidden-items")
 
   icon.addEventListener("click",() =>{
     if (!nav.classList.contains("responsive")){
@@ -11,6 +14,26 @@ function navigation() {
       nav.classList.remove("responsive")
     }
 
+  });
+
+  lessonList.addEventListener("click",()=>{
+    if (!hiddenItems[0].classList.contains("lesson-items")){
+      hiddenItems[0].style.display = "none";
+      hiddenItems[0].classList.add("lesson-items");
+    } else{
+      hiddenItems[0].style.display = "block";
+      hiddenItems[0].classList.remove("lesson-items");
+    }
+  });
+
+  articleList.addEventListener("click",()=>{
+    if (!hiddenItems[1].classList.contains("article-items")){
+      hiddenItems[1].style.display = "none";
+      hiddenItems[1].classList.add("article-items");
+    } else{
+      hiddenItems[1].style.display = "block";
+      hiddenItems[1].classList.remove("article-items");
+    }
   });
 }
 
